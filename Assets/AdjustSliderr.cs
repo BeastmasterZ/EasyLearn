292c7c1e537058e3243c7449cc4384041739dd81
Harish Kumar <hk9138@gmail.com>
2021-06-19 05:10:24 +0530

    first

---
+++ Assets/AdjustSliderr.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.ComponentModel;
+
+
+public class AdjustSliderr : MonoBehaviour
+{
+    public float rotationMin = 0.0f;
+    public float rotationMax = 45.0f;
+    public Slider RotateSlider;
+    public Slider ScaleSlider;
+
+    public float currentRotation = 0f;
+    public float currentScale = 0.1f;
+
+    void start()
+    {
+        RotateSlider = GameObject.Find("RotateSlider").GetComponent<Slider>();
+        ScaleSlider = GameObject.Find("ScaleSlider").GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0.0f, RotateSlider.value, 0.0f);
+        transform.localScale = new Vector3(ScaleSlider.value, ScaleSlider.value, ScaleSlider.value);
+    }
+
+    
+    public void AdjustAngle(float newAngle)
+    { }
+    public void AdjustScale(float newScale)
+    { }
+}
